da2fe8f98e745da58edb77046d465039aca0be85
bvikash363R <bvikash363@gmail.com>
2025-05-04 14:10:15 +0530

    Node package upload

---
+++ Node/TreeNode.java
@@ -0,0 +1,17 @@
+package Node;
+
+public class TreeNode {
+    public int data;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public int get() {
+        return data;
+    }
+}
